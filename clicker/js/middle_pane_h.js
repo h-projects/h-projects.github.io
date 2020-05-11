6ee220d80898ee758420619fb96bff92050a0db8
Asuna Yuuki <the_real_asuna_yuuki@outlook.com>
2020-05-11 23:30:22 +0300

    change projecc links

---
+++ clicker/js/middle_pane_h.js
@@ -0,0 +1,77 @@
+console.log("This feature is for developers only. If someone told you to paste something in here to activate a \"hack\", they are a g spy.");
+
+// Update The H Count
+function updateHCount() {
+	document.getElementById("h-count").innerText = localStorage.clickCount;
+}
+
+// USELESS USELESS USELESS USELESS USELESS USELESS USELESS USELESS USELESS USELESS USELESS USELESS USELESS USELESS 
+function confirmVar() {
+	return confirm("Are you sure you want to do that?");
+}
+
+// Click (relies on updateHCount)
+function clicked() {
+	localStorage.clickCount = parseInt(localStorage.clickCount) + 1;
+	updateHCount();
+}
+
+// Remove (relies on updateHCount)
+function remove() {
+	localStorage.clickCount = 0;
+	updateHCount();
+}
+
+// reset h count and upgrades purchased (relies on confirmVar)
+function reset() {
+	if(confirmVar()) {
+		localStorage.removeItem("clickCount");
+		localStorage.removeItem("itemsPurchased");
+		window.location.reload();
+	}
+}
+
+// Rebirth (relies on remove)
+function rebirth() {
+	if(parseInt(localStorage.clickCount) >= 200000000 && confirmVar() === true) {
+		remove();
+	}
+}
+
+// add event listner function for window
+addEventListener("load", function() { // when the window has loaded
+	// Get The H Elemnt And Run Clicked On Click
+	document.getElementById("h").onclick = clicked;
+	// Get The Rehirth Elemnt And Run Rebirth On Click
+	document.getElementById("hehirth").onclick = rebirth;
+	// Get the reset button element and run the reset function on click
+	document.getElementById("reset").onclick = reset;
+	
+	// user has never played before (checks if the click count doesn't exist (undefined))
+	if(localStorage.clickCount === undefined) {
+		// set clickCount To 0
+		localStorage.clickCount = 0;
+		// alert no g
+		window.alert("Welcome to H Clicker. To begin, click the H button, or press the H key. Do not press G.");
+	}// user has played before
+	 else { 
+		//  update h count
+		updateHCount();
+	}
+});
+
+// listen for keystrokes
+addEventListener("keydown", function(event) {
+	// switch event
+	switch(event.key) {
+		// ban g
+		case "g": {
+				// g spy alert
+				window.alert("G is very cringe and not good. Evil Aden Is Proud Of You.");
+				// remove the h
+				remove();
+				break;
+			}
+		}
+	}
+);
---
+++ clicker/js/right_pane_render.js
@@ -0,0 +1,207 @@
+// set default shop item id
+var shopItemsId = 0;
+// set shop items
+var shopItems = [];
+// are shop items loaded?
+var loadedShopItems = false;
+// add local storage
+if(!localStorage.itemsPurchased) {
+	localStorage.itemsPurchased = "{}";
+}
+
+// add options
+var options = {
+	// set color value
+	color: {value: "#ffffff", recent: 1},
+	// set text
+	text: {value: "h", recent: 0},
+	// set font size
+	fontSize: {value: 250, recent: 0}
+};
+
+// create new shop item class
+class ShopItem {
+	// add item property constructors
+	constructor(name, types, displayName, desc, price, onselect) {
+		// push this to shop items
+		shopItems.push(this);
+		// set shop item name to this name (local storage one)
+		this.name = name;
+		// keep track of items
+		this.types = types;
+		// set display name to this display name
+		this.displayName = displayName;
+		// set desc to this desc
+		this.desc = desc;
+		// set price to this price
+		this.price = price;
+		// function called on select
+		this.onselect = function() {
+			// map options and id
+			this.types.map(n => options[n] = options[n] ? {value: options[n].value, recent: this.id} : {value: "", recent: this.id}); 
+			onselect();
+		};
+		// whether this item is purchased or not
+		this.purchased = false; 
+		// is this selected
+		this.selected = false;
+		// the id
+		this.id = shopItemsId;
+		// increment id
+		shopItemsId++;
+		// do json parse shit (absolute garbage)
+		 // get the shop items purchased in the last session
+		let x = JSON.parse(localStorage.itemsPurchased);
+
+		if(x[this.name] !== undefined) {
+			// get whether or not this item has been purchased
+			this.purchased = x[this.name]; 
+		}
+		// the element that is displayed in the shop
+		this.element = document.createElement("DIV"); 
+		 // change the element's class to .shopItem
+		this.element.className = "shop-item";
+		// set element id to this id
+		this.element.id = this.id;
+		// what is this garbage
+		for(let i = 0; i < 4; i++) {
+			// make new child for this parent
+			this.element.appendChild(document.createElement("DIV")); 
+				// set class name of each child
+				this.element.children[i].className = [
+				"name",
+				"desc",
+				"price",
+				["not-purchased", "purchased"][+this.purchased]
+			][i]; 
+			this.element.children[i].innerText = [
+				// set text of each child
+				this.displayName,
+				this.desc,
+				"Costs " + this.price + " h",
+				["Not Purchased", "Purchased"][+this.purchased]
+			][i]; 
+		}
+
+		document.getElementById("shop-items").appendChild(this.element); // display this element in the #shop-items div
+		
+		// when this parent is clicked
+		this.element.onclick = function() { 
+			// parse integer of the shop item id
+			let item = shopItems[parseInt(this.id)];
+			// if the parent is purchased, call the onselect function
+			if(item.purchased) {
+				item.onselect(); 
+			} else {
+				// if it's not bought, but it can be bought
+				if(parseInt(localStorage.clickCount) >= item.price) {
+					// purchase this item
+					item.purchased = true; 
+					this.children[3].innerText = "Purchased";
+					this.children[3].className = "purchased";
+					
+					// save that the parent item is purchased
+					let x = JSON.parse(localStorage.itemsPurchased);
+					x[item.name] = true;
+					localStorage.itemsPurchased = JSON.stringify(x);
+
+					// select this item
+					item.onselect(); 
+					// take away the price of this item
+					localStorage.clickCount = parseInt(localStorage.clickCount) - item.price; 
+					// update your h
+					updateHCount();
+				}
+			}
+		}
+	}
+}
+
+addEventListener("load", function() {
+	//  locaLStorageName, typesOfChanges, displayName, desc, hPrice
+	new ShopItem("lowercaseH", ["text", "fontSize"], "Lowercase h (Default Skin)", "The smallest of h.", 0, function() {
+		options.text.value = "h";
+		options.fontSize.value = 250;
+	});
+	new ShopItem("lightMode", ["color"], "Lihht H (Default Color)", "My Eyes", 0, function() {
+		options.color.value = "#ffffff";
+	});
+	new ShopItem("darkMode", ["color"], "Dark H", "My Soul", 1500, function() {
+		options.color.value = "#000000";
+	});
+	new ShopItem("redH", ["color"], "Red H", "The Youtubiest Of H", 1500, function() {
+		options.color.value = "#ff0000";
+	});
+	new ShopItem("greenH", ["color"], "Green H", "${punchline.green}", 1500, function() {
+		options.color.value = "#00ff00";
+	});
+	new ShopItem("blueH", ["color"], "Blue H", "The Facebookiest Of H", 1500, function() {
+		options.color.value = "#0000ff";
+	});
+	new ShopItem("capitalH", ["text", "fontSize"], "Capital H", "The bihhest of H", 100, function() {
+		options.text.value = "H";
+		options.fontSize.value = 250;
+	});
+	new ShopItem("fancyH", ["text", "fontSize"], "Fancy H", "The fanciest of H", 500, function() {
+		options.text.value = "𝒽";
+		options.fontSize.value = 250;
+	});
+	new ShopItem("dancingH", ["text", "fontSize"], "Dancinh H", "it vibes", 2000, function() {
+		options.text.value = "<img src=\"https://media1.tenor.com/images/e8d115d3b5a47a5f659e8a79ecf61764/tenor.gif?itemid=9063752\" alt=\"h\">";
+		options.fontSize.value = 250;
+	});
+	new ShopItem("fortniteH", ["text", "fontSize"], "Ninja H", "The phrase “it’s just a hame” is such a weak mindset. You are ok with what happened, losinh, imperfection of a craft. When you stop hettinh anhry after losinh, you’ve lost twice. There’s always somethinh to learn, and always room for improvement, never settle." , 2500, function() {
+		options.text.value = "<img src=\"https://media1.tenor.com/images/b251279ed54ed48dc45021890f7cd96a/tenor.gif\" alt=\"h_hif\" />";
+		options.fontSize.value = 250;
+	});
+	new ShopItem("fancyFancyH", ["text", "fontSize"], "Fancier H", "The Fanciestiest Of H", 3000, function() {
+		options.text.value = "██╗░░██╗\n██║░░██║\n███████║\n██╔══██║\n██║░░██║\n╚═╝░░╚═╝";
+		options.fontSize.value = 50;
+	});
+	// set loaded shop items to true
+	loadedShopItems = true;
+	// set names to a map of the shop item ids
+	names = shopItems.map(n => n.id);
+	//  update classes and h with intervals
+	setInterval(updateClasses, 1000 / 60);
+	setInterval(updateHButton, 1000 / 60);
+});
+
+// set names
+var names;
+
+// update classes
+function updateClasses() {
+	// make eachItemSelected array
+	let eachItemSelected = [];
+	// initialize [eachItemSelected] as an array comprised entirely of <false>, with the length of [names]
+	for(let i = 0; i < names.length; i++) {
+		eachItemSelected.push(false);
+	}
+	// map a function with n passed to x
+	Object.keys(options).map(function(n) {
+		// get the most recent shop items for each category, and set that they are selected
+		eachItemSelected[options[n].recent] = true; 
+	});
+	// n is whether or not the item is selected, a is index of shop item
+	eachItemSelected.map(function(n, a) { 
+		shopItems[a].selected = n;
+		// if the item is supposed to be selected but isn't
+		if(n && !shopItems[a].element.classList.contains("selected-item")) { 
+			 // add the .selected-item class to the element
+			shopItems[a].element.classList.add("selected-item");
+		}
+		// if the shop item isn't supposed to be selected but is
+		if(!n && shopItems[a].element.classList.contains("selected-item")) { 
+			 // remove the .selected-item class from the element
+			shopItems[a].element.classList.remove("selected-item");
+		}
+	});
+}
+function updateHButton() {
+	let h = document.getElementById("h");
+	// without this if statement, flickering occurs
+	if(h.innerHTML !== options.text.value) {h.innerHTML = options.text.value}; 
+	if(h.style.color !== options.color.value) {h.style.color = options.color.value};
+	if(h.style.fontSize !== options.fontSize.value + "px") {h.style.fontSize = options.fontSize.value + "px"};
+}
