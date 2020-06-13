a43c269dbd1eca774ff5e7c7056cf653e88e1547
Senko <32780515+Senko-Dev@users.noreply.github.com>
2020-06-13 16:28:40 +0300

    yes

--- common/404.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("hh").style.display = "flex";
-
-setTimeout(function() {
-  document.getElementById("h").style.display = "flex";
-  document.getElementById("hh").style.display = "none";
-}, 400);
