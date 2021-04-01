f64a2cf6c214f1441d75931d72b3ff24d7a72db0
vooxal <58456527+vooxal@users.noreply.github.com>
2021-03-31 17:20:50 -0700

    Fixes #13

--- app/fuck-g/fuck-g.js
+++ app/fuck-g/fuck-g.js
@@ -1,52 +1,52 @@
-function get(id) {
+function qet(id) {
 	return document.getElementById(id);
 }
 
-get("to-q").onclick = function() {
-	get("output").value = get("input").value.split("g").join("q").split("G").join("Q");
+qet("to-q").onclick = function() {
+	qet("output").value = qet("input").value.split("g").join("q").split("G").join("Q");
 }
 
-get("to-soft-h").onclick = function() {
-	get("output").value = get("input").value.split("g").join("ĥ").split("G").join("Ĥ");
-}
-
-
-get("to-hard-h").onclick = function() {
-	get("output").value = get("input").value.split("g").join("h").split("G").join("H");
+qet("to-soft-h").onclick = function() {
+	qet("output").value = qet("input").value.split("g").join("ĥ").split("G").join("Ĥ");
 }
 
 
-get("copy").onclick = function () {
+qet("to-hard-h").onclick = function() {
+	qet("output").value = qet("input").value.split("g").join("h").split("G").join("H");
+}
+
+
+qet("copy").onclick = function () {
 	/* Select the text field */
-	get("output").select();
-	get("output").setSelectionRange(0, 99999); /*For mobile devices*/
+	qet("output").select();
+	qet("output").setSelectionRange(0, 99999); /*For mobile devices*/
   
 	/* Copy the text inside the text field */
 	document.execCommand("copy");
 }
-/* const select = get("selector");
-get("input").oninput= function() {
+/* const select = qet("selector");
+qet("input").oninput= function() {
     if(select.value = "to-q") {
-        get("output").value = get("input").value.split("g").join("q").split("G").join("Q");
+        qet("output").value = qet("input").value.split("g").join("q").split("G").join("Q");
     }
     else if(select.value = "to-soft-h") {
-        get("output").value = get("input").value.split("g").join("ĥ").split("G").join("Ĥ");
+        qet("output").value = qet("input").value.split("g").join("ĥ").split("G").join("Ĥ");
     }
     else if(select.value = "to-hard-h") {
-        get("output").value = get("input").value.split("g").join("h").split("G").join("H");
+        qet("output").value = qet("input").value.split("g").join("h").split("G").join("H");
     }
 }
 
-get("select").onselect = function() {
-    get("input").oninput= function() {
+qet("select").onselect = function() {
+    qet("input").oninput= function() {
         if(select.value = "to-q") {
-            get("output").value = get("input").value.split("g").join("q").split("G").join("Q");
+            qet("output").value = qet("input").value.split("g").join("q").split("G").join("Q");
         }
         else if(select.value = "to-soft-h") {
-            get("output").value = get("input").value.split("g").join("ĥ").split("G").join("Ĥ");
+            qet("output").value = qet("input").value.split("g").join("ĥ").split("G").join("Ĥ");
         }
         else if(select.value = "to-hard-h") {
-            get("output").value = get("input").value.split("g").join("h").split("G").join("H");
+            qet("output").value = qet("input").value.split("g").join("h").split("G").join("H");
         }
     }
 } */
